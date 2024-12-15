4b5d623fb5e5f8c67db5b492efdefa8241a43fee
temcheeter <akravchyg10@mail.ru>
2024-12-15 19:22:28 +0300

    almost ready

--- prediction.py
+++ prediction.py
@@ -1,8 +1,28 @@
-from aiogram import Router, F
+from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
+import textwrap
+import pytz
+import json
 
 
-rt = Router()
+async def predict(predict_date: str):
+    img = Image.open('source.png')
+    idraw = ImageDraw.Draw(img)
+    headline = ImageFont.truetype('century_bold.ttf', size=90)
+    forecast = ImageFont.truetype('century_regular.ttf', size=48)
+    dateline = ImageFont.truetype('Segoe-UI-Variable-Static-Text-Semibold.ttf', size=42)
 
+    with open('forecasts.json', 'r', encoding='utf-8') as f:
+        dictionary = json.load(f)
+        text = dictionary[predict_date]
+        wrapped_text = textwrap.fill(text, width=20)
+    time = datetime.now(pytz.timezone('Europe/Moscow')).strftime('%H:%M')
+    time = time[:-1] + str(int(time[-1]) - 1)
+    date = datetime.now(pytz.timezone('Europe/Moscow')).strftime('%d.%m')
 
-@rt.message(F.text == 'Начать💥')
-async def
+    idraw.text((414, 500), '17 января', 'black', font=headline, anchor='mm')
+    idraw.text((420, 780), wrapped_text, font=forecast, fill='#33273c', anchor='mm')
+    idraw.text((15, 1743), date, font=dateline, anchor='lm')
+    idraw.text((215, 1743), time, font=dateline, anchor='rm')
+    img.show()
+
